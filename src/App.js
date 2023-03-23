1bd8e355d806c57fbb7f13d63e24fdb52c2b4030
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-03-23 20:25:33 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 export default function App() {
   const [chekvalue, setCheckValue] = useState(false);
   const [values, setValues] = useState({
     firstchk: chekvalue,
   });
-  const [isSubmit, setIsSubmit] = useState(false);
   const handleChange = (e) => {
+    console.log(e.target.checked);
     setCheckValue(e.target.checked);
     const name = e.target.name;
     const value = e.target.checked;
     setValues({ ...values, [name]: value });
     console.log('values of check click' + chekvalue);
-  };
-  const submitdata = (e) => {
-    e.preventDefault();
-    setIsSubmit(true);
-    if (!isSubmit) {
-      console.log('israr hey');
-      console.log('values of check click' + chekvalue);
-      //console.log('values of check click innner' + values.firstchk);
-      const allData = { ...values };
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(allData),
-      };
-      console.log(requestOptions.body);
-      fetch('https://reqres.in/api/posts', requestOptions)
-        .then((response) => {
-          console.log('post sucess' + response);
-          response.json();
-        })
-        .catch((error) => {
-          console.log('failed result' + error);
-        });
-    }
+    console.log('values of check click innner' + values.firstchk);
   };
   return (
-    <form onSubmit={submitdata}>
+    <div>
       <h1>Hello StackBlitz!</h1>
       <p>
         <input
@@ -48,10 +25,7 @@ export default function App() {
           name="firstChk"
         ></input>
       </p>
-      <p>
-        <input type="submit" />
-      </p>
-    </form>
+    </div>
   );
 }
 //There should be a parent component and a child component
