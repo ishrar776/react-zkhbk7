acd76000ed4e927971fcec0beb429daf25e0282b
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-03-23 17:40:59 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React, { useState } from 'react';
 import './style.css';
 export default function App() {
   const [chekvalue, setCheckValue] = useState(false);
-  //const [chekvalue1, setCheckValue1] = useState(false);
   const [values, setValues] = useState({
     firstchk: chekvalue,
-    //firstchk1: chekvalue1,
   });
   const [isSubmit, setIsSubmit] = useState(false);
   const handleChange = (e) => {
@@ -13,15 +11,7 @@ export default function App() {
     const name = e.target.name;
     const value = e.target.checked;
     setValues({ ...values, [name]: value });
-    //console.log('values of checkbox is' + chekvalue);
-    //console.log('revaryvay' + values.firstchk);
   };
-  // const handleChange1 = (e) => {
-  //   setCheckValue1(e.target.checked);
-  //   const name = e.target.name;
-  //   const value = e.target.checked;
-  //   setValues({ ...values, [name]: value });
-  // };
   const submitdata = (e) => {
     e.preventDefault();
     setIsSubmit(true);
@@ -40,9 +30,7 @@ export default function App() {
         .then((response) => {
           console.log('post sucess' + response);
           response.json();
-          //navigate('/register');
         })
-        //.then((data) => setValues(data.message))
         .catch((error) => {
           console.log('failed result' + error);
         });
@@ -59,14 +47,6 @@ export default function App() {
           name="firstChk"
         ></input>
       </p>
-      {/* <p>
-        <input
-          type="checkbox"
-          onChange={handleChange1}
-          value={values.firstchk1}
-          name="firstChk1"
-        ></input>
-      </p> */}
       <p>
         <input type="submit" />
       </p>
